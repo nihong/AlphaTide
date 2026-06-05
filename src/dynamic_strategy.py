5e01be63e1dbc5d09456fb8baad41381376cf3b3
gaopeng <gaopeng@mgtv.com>
2026-06-05 23:54:46 +0800

    Auto-Gen Logic (Global Macro): Ret: -1.47%, DD: 72.55%

--- src/dynamic_strategy.py
+++ src/dynamic_strategy.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 def generate_signals(df):
+    df['ma5'] = df['收盘'].rolling(5).mean()
     df['ma10'] = df['收盘'].rolling(10).mean()
     df['ma20'] = df['收盘'].rolling(20).mean()
     df['ma60'] = df['收盘'].rolling(60).mean()
@@ -14,11 +15,11 @@ def generate_signals(df):
     loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
     rs = gain / loss
     df['rsi'] = 100 - (100 / (1 + rs))
-    buy_signal = ((df['收盘'] > df['ma60']) & (df['macd'] > df['macd_sig'])) & ((df['ma20'] > df['ma60']) & (df['ma20'].shift(1) <= df['ma60'].shift(1)))
+    buy_signal = ((df['rsi'] < 30)) & ((df['macd'] > df['macd_sig']) & (df['macd'].shift(1) <= df['macd_sig'].shift(1)))
     tr1 = df['最高'] - df['最低']
     tr2 = (df['最高'] - df['收盘'].shift(1)).abs()
     tr3 = (df['最低'] - df['收盘'].shift(1)).abs()
     tr = pd.concat([tr1, tr2, tr3], axis=1).max(axis=1)
     atr = tr.rolling(14).mean()
-    stop_distance = atr * 1.837114146134537
+    stop_distance = atr * 0.996960736116727
     return buy_signal, stop_distance
