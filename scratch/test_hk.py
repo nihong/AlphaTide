fdf5ba5c657c0d9a6a30ccb2ddef2405eb0cdb69
roygao_work <gaopeng@mgtv.com>
2026-06-08 12:43:36 +0800

    chore: deep clean repository, archive legacy managers and tests, remove scratch files

--- scratch/test_hk.py
+++
@@ -1,8 +0,0 @@
-import akshare as ak
-
-print("Testing HK Sina...")
-try:
-    df_hk = ak.stock_hk_daily(symbol="00700")
-    print(df_hk.tail(3))
-except Exception as e:
-    print("Error Sina:", e)
